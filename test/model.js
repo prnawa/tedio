bc23384396ccea98905797755276c83f9ffd234d
Ruwan Nawarathnage <ruwan.nawarathnage@asiarooms.com>
2014-07-09 18:17:54 +0800

    adding unit tests for model

---
+++ test/model.js
@@ -0,0 +1,17 @@
+var proxyquire = require('proxyquire');
+
+describe('model', function() {
+
+    var getModel = function(options){
+        return proxyquire('../lib/model', {})(options);
+    };
+
+    it('should merge default with provided options', function(done) {
+        var expectedTable = 'test';
+        var expectedId = 'INTEGER';
+        var model = getModel({table: expectedTable, columns: {id : expectedId}});
+        model.table.should.equal(expectedTable);
+        model.columns.id.should.equal(expectedId);
+        done();
+    });
+});
