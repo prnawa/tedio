61ac62a33f522663bce1f01e9ad5daa63c3158d9
Ruwan Nawarathne <prnawa@gmail.com>
2014-07-12 15:01:34 +0800

    Implementation of criteria object
    added functionality - adding new restrictions, chaining of adding
    restrictions.

---
+++ test/criteria.js
@@ -0,0 +1,49 @@
+var proxyquire = require('proxyquire').noCallThru();
+
+describe('criteria', function() {
+
+    var getCriteria = function() {
+        return proxyquire('../lib/criteria', {});
+    };
+    it('should expose create method to create new criteria', function(done) {
+        var criteria = getCriteria();
+        expect(_.isFunction(criteria.create)).to.be.true;
+        done();
+    });
+
+    describe('criteria.create', function() {
+        it('should returns an object', function(done) {
+            var criteria = getCriteria();
+            expect(_.isObject(criteria.create())).to.be.true;
+            done();
+        });
+
+        it('should returns an object with add method to add new restrictions', function(done) {
+            var criteria = getCriteria();
+            expect(_.isFunction(criteria.create().add)).to.be.true;
+            done();
+        });
+    });
+
+    describe('add restrictions', function() {
+        it('should add provided restrictions to criteria\'s restrictions', function(done) {
+            var expr1 = {};
+            var criteria = getCriteria();
+            criteria = criteria.create().add(expr1);
+            expect(criteria.restrictions.length).to.be.equal(1);
+            expect(criteria.restrictions[0]).to.be.equal(expr1);
+            done();
+        });
+
+        it('should be able to chain to add multiple restrictions', function(done) {
+            var expr1 = {};
+            var expr2 = {};
+            var criteria = getCriteria();
+            criteria = criteria.create().add(expr1).add(expr2);
+            expect(criteria.restrictions.length).to.be.equal(2);
+            expect(criteria.restrictions[0]).to.be.equal(expr1);
+            expect(criteria.restrictions[1]).to.be.equal(expr2);
+            done();
+        });
+    });
+})
